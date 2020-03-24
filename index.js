239df4ce64f626ccb45f29bc37b4580f63955cc6
Yusra Atta <yvvsra1@gmail.com>
2020-03-24 08:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+
+function createEmployeeRecord(array){
+return {
+  firstName: array[0],
+  familyName:array[1],
+  title :array[2],
+  payPerHour:array[3],
+  timeInEvents:[],
+  timeOutEvents: []
+}
+}
+
+function createEmployeeRecords(array){
+  return array.map(function(record){
+    return createEmployeeRecord(record)  
+  })
+}
